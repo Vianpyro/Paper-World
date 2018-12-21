40f978525ad039839b467b39f1e90f2b756bd600
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2018-12-21 20:45:05 +0100

    Camera optimisation

--- Alpha 1/src/dev/vianpyro/paperworld/worlds/World.java
+++ Alpha 1/src/dev/vianpyro/paperworld/worlds/World.java
@@ -22,8 +22,13 @@ public class World {
 	}
 	
 	public void render(Graphics g) {
-		for(int y = 0; y < height; y++) {
-			for(int x = 0; x < width; x++) {
+		int xStart = (int)Math.max(0, game.getGameCamera().getxOffset() / Tile.DEFAULT_TILE_WIDTH),
+			xEnd = (int)Math.min(width, (game.getGameCamera().getxOffset() + game.getWidth()) / Tile.DEFAULT_TILE_WIDTH) + 1, 
+			yStart = (int)Math.max(0, game.getGameCamera().getyOffset() / Tile.DEFAULT_TILE_HEIGHT), 
+			yEnd = (int)Math.min(height, (game.getGameCamera().getyOffset() + game.getHeight()) / Tile.DEFAULT_TILE_HEIGHT) + 1;
+		
+		for(int y = yStart; y < yEnd; y++) {
+			for(int x = xStart; x < xEnd; x++) {
 				getTile(x, y).render(g, (int)(x * Tile.DEFAULT_TILE_WIDTH - game.getGameCamera().getxOffset()), (int)(y * Tile.DEFAULT_TILE_HEIGHT - game.getGameCamera().getyOffset()));
 			}
 		}
